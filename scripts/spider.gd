8334248314aac424e32bc26f633dbbe43463823b
justfordoener <46346088+justfordoener@users.noreply.github.com>
2025-01-24 21:16:24 +0100

    level structure

---
+++ scripts/spider.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if (Input.is_action_pressed("move_left")):
+		transform.global_position += Vector3(1, 1, 1) * delta
