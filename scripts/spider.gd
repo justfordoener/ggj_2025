53278c542cdc33a42077034b3885e574e2019d05
justfordoener <46346088+justfordoener@users.noreply.github.com>
2025-01-24 21:59:22 +0100

    basic movement

--- scripts/spider.gd
+++ scripts/spider.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
+@export var speed = 0.1
+@export var rotation_speed = 1
+var orientation : Vector3 = Vector3(1,0,0)
+var direction : Vector3 = Vector3.ZERO
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -7,6 +11,18 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	if (Input.is_action_pressed("move_left")):
-		transform.global_position += Vector3(1, 1, 1) * delta
+func _physics_process(delta: float) -> void:
+	_move(delta)
+	
+func _move(delta : float):
+	direction = Vector3.ZERO
+	if Input.is_action_pressed("move_right"):
+		direction += Vector3(1,0,0)		
+	if Input.is_action_pressed("move_left"):
+		direction -= Vector3(1,0,0)
+	if Input.is_action_pressed("move_up"):
+		direction -= Vector3(0,0,1)
+	if Input.is_action_pressed("move_down"):
+		direction += Vector3(0,0,1)	
+	position += direction * speed * delta
+	look_at(global_position + direction, Vector3.UP)
