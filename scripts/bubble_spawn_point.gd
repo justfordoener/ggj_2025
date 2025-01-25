28ef965d441ba73728a898785334540d32d4c003
fimonsischer <simfisch99@gmx.de>
2025-01-25 14:45:32 +0100

    spawner in level, variables accessible

--- scripts/bubble_spawn_point.gd
+++ scripts/bubble_spawn_point.gd
@@ -2,12 +2,17 @@ extends Node3D
 
 var time_left : float = 0
 
-@onready var cooldown : float = $bubble.cooldown
+@export var start_scale: float = 0.05
+@export var max_scale: float = 0.3
+@export var growth_rate: float = 0.02
+@export var cooldown : float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$bubble.start_scale = start_scale
+	$bubble.max_scale = max_scale
+	$bubble.growth_rate = growth_rate
+	$bubble.cooldown = cooldown
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
