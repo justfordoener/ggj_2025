6a904308e6a6f91f9b27fe2b7791d703f378a13f
justfordoener <46346088+justfordoener@users.noreply.github.com>
2025-01-25 16:10:33 +0100

    bubble float up

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -5,15 +5,18 @@ extends Node3D
 @export var max_scale: float = 0.3
 @export var growth_rate: float = 0.02
 @export var cooldown : float = 0
+@export var time_to_pop : float = 10
+@export var float_speed : float = 0.6
 var air_amount: float = 0.0
 var has_spawned : bool = false
+var is_full : bool = false
+
+@onready var timer : Timer = $Timer
 #onready var collision_shape = $Area3D/CollisionShape3D
 
-# signal for popping
-signal bubble_popped
-signal bubble_absorbed
-
 func _ready():
+	timer.wait_time = time_to_pop 
+	timer.one_shot = true
 	scale = Vector3.ONE * start_scale			# small starting bubble
 	await get_tree().create_timer(cooldown).timeout
 	has_spawned = true
@@ -28,22 +31,24 @@ func _process(delta: float):
 	if not has_spawned:
 		return
 	# bubble grows
-	scale += Vector3.ONE * growth_rate * delta
-	air_amount = calculate_air_amount(scale)
+	if not is_full:
+		scale += Vector3.ONE * growth_rate * delta
+		air_amount = calculate_air_amount(scale)
 	
 	# check if bubble popps
 	if scale >= Vector3.ONE * max_scale:
-		pop_bubble()
+		pop_bubble(delta)
 		
 
 func calculate_air_amount(cur_scale: Vector3):
 	return 4.0 / 3.0 * PI * pow(cur_scale.x, 3) * 1000
 
-func pop_bubble():
-	# emitt signal and play pop animation
-	emit_signal("bubble_popped")
-	print("Bubble popped")
-	
+func pop_bubble(delta : float):
+	if not is_full:
+		is_full = true
+		timer.start()
+	global_position += Vector3(0,1,0) * delta * float_speed	
+func _on_timer_timeout():
 	queue_free()
 	
 func absorb_bubble() -> float:
