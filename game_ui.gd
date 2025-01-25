aaa407c77147027b1c9a6998aff7ad05fc400ee5
Léo <siggi@brucker.fr>
2025-01-25 16:44:34 +0100

    before crash?

--- game_ui.gd
+++ game_ui.gd
@@ -1,11 +1,26 @@
 extends Control
 
+@onready var game_over_menu = $"game over menu"
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var level = get_parent()
+	var spiders_node = level.get_node("spiders")
+	
+	game_over_menu.visible = false
+	
+	if spiders_node:
+		for spider in spiders_node.get_children():
+			if spider.has_signal("spider_died"):
+				spider.connect("spider_died", Callable(self, "_on_spider_died"))
+	else:
+		print("Spiders node not found!")
 
-
+func _on_spider_died(name):
+	print("UI received signal: Spider " + name + " died!")
+	game_over_menu.visible = true
+	
+	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
