d3a6143b27949fcfd240d1be827a1c2896a02fda
Léo <siggi@brucker.fr>
2025-01-25 18:32:09 +0100

    Add all final menu stuff (working)

--- examples/scenes/overlaid_menus/overlaid_menu.gd
+++ examples/scenes/overlaid_menus/overlaid_menu.gd
@@ -10,8 +10,8 @@ func _on_restart_button_pressed():
 func _on_main_menu_button_pressed():
 	SceneLoader.load_scene(main_menu_path)
 	
-func _set_text(dead_spider):
-	if dead_spider:
-		displayText.text = "Game Over.. Your friend " + dead_spider + " suffocated.."
-	else:
-		displayText.text = "Game Won!! The Air-ambulance saved everyone!!"
+func set_text_loss(dead_spider):
+	displayText.text = "Game Over.. Your friend " + dead_spider + " suffocated.."
+
+func set_text_win():
+	displayText.text = "Game Won!! The Air-ambulance saved everyone!!"
--- scripts/game_ui.gd
+++ scripts/game_ui.gd
@@ -1,12 +1,15 @@
 extends Control
 
 @onready var game_over_menu = $"game over menu"
-
+@onready var game_timer = $"game timer"
 func _ready():
+	get_tree().paused = false
 	var level = get_parent()
 	var spiders_node = level.get_node("spiders")
 	
 	game_over_menu.visible = false
+	
+	game_timer.connect("ambulance_is_there", Callable(self, "_on_ambulance_is_there"))
 	
 	if spiders_node:
 		for spider in spiders_node.get_children():
@@ -18,10 +21,11 @@ func _ready():
 func _on_spider_died(name):
 	print("UI received signal: Spider " + name + " died!")
 	game_over_menu.visible = true
-	game_over_menu._set_text(name)
+	game_over_menu.set_text_loss(name)
+	get_tree().paused = true
 
-func _on_game_timer_finished():
+func _on_ambulance_is_there():
 	print("UI received signal: Timer is finished!")
-	
 	game_over_menu.visible = true
-	game_over_menu.set_text()
+	game_over_menu.set_text_win()
+	get_tree().paused = true
